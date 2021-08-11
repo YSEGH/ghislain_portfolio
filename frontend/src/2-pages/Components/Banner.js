fdd75b6db8b12ee2a2d63efc1b0908c96a0a7af7
youssef <youssef.segh@hotmail.fr>
2021-08-11 14:43:13 +0200

    push heroku

--- frontend/src/2-pages/Components/Banner.js
+++ frontend/src/2-pages/Components/Banner.js
@@ -24,6 +24,7 @@ export default function Banner() {
 
   useEffect(() => {
     var video = document.querySelector(".video-banner");
+    video.muted = true;
     video.play();
     /*     video.addEventListener("loadeddata", () => checkLoaded(video));
      */
